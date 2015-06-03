60a212d4e29cff68479be45beb0f4d18fff4ed31
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2015-06-03 18:45:17 +0200

    Fixing some defects

--- src/main/java/foo/Simplest.java
+++ src/main/java/foo/Simplest.java
@@ -6,7 +6,6 @@ package foo;
 public class Simplest {
 
 	public String s;
-	public String s2;
 
     public static int add(int a, int b) {
     	// introduce a variable that is not needed - just to get a violation
