02afd79dd02f144c99f2051425e2a5ec2f203969
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2015-06-03 14:28:27 +0200

    Add some code which should contain quality flaws

--- src/main/java/foo/Simplest.java
+++ src/main/java/foo/Simplest.java
@@ -5,8 +5,12 @@ package foo;
  */
 public class Simplest {
 
+	public String s;
+
     public static int add(int a, int b) {
     	// introduce a variable that is not needed - just to get a violation
+		int i;
+
     	int result = a + b;
 
     	System.out.println("");
