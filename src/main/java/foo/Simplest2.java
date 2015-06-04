014f3bcde057c95c76426f187421d423915393cb
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2015-06-04 15:58:44 +0200

    Write some ugly code

--- src/main/java/foo/Simplest2.java
+++ src/main/java/foo/Simplest2.java
@@ -4,10 +4,10 @@ public class Simplest2 {
 
 	public static void foo() {
 		// the following lines will create a blocker issue during a SQ analysis
-		/*
+		
 		Object o = null;
 		int i = o.hashCode();
-		*/
+		
 
 		System.out.println("");
 	}
