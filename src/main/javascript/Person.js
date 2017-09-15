9b999dd88223998f5563c99884c5580d14ddc3cc
Fabrice Bellingard <bellingard@gmail.com>
2017-09-15 11:19:46 +0200

    Update Person.js

--- src/main/javascript/Person.js
+++ src/main/javascript/Person.js
@@ -3,12 +3,13 @@ var Person = function(first, last, middle) {
     this.first = first;
     this.middle = middle;
     this.last = last;
+    this.name = this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last
 };
 
 Person.prototype = {
 
     whoAreYou : function() {
-        return this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last;
+        return this.name;
     }
 
 };
