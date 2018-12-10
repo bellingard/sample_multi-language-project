efdd8e374c9a70ade7ee293c487e072f6fa725d2
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2018-12-10 18:17:27 +0100

    Try to add some Java code to be analyzed...

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -5,6 +5,8 @@ package foo;
  */
 public class Foo {
 
+    public int a;
+
     public static int div(int a, int b) throws Exception {
     	if (b == 0) {
     		throw new UnsupportedOperationException("Can't divide by zero!");
