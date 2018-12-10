9e1bb5c87b6e86864af32174ed90bf4f5b8e67b6
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2018-12-10 17:33:19 +0100

    Simplify big file

--- src/main/php/index.php
+++ src/main/php/index.php
@@ -2,14 +2,6 @@
 
 namespace PHPMailer\PHPMailer;
 
-/**
- * PHPMailer - PHP email creation and transport class.
- *
- * @author  Marcus Bointon (Synchro/coolbru) <phpmailer@synchromedia.co.uk>
- * @author  Jim Jagielski (jimjag) <jimjag@gmail.com>
- * @author  Andy Prevost (codeworxtech) <codeworxtech@users.sourceforge.net>
- * @author  Brent R. Matzelle (original founder)
- */
 class PHPMailer
 {
     const ENCODING_QUOTED_PRINTABLE = 'quoted-printable';
